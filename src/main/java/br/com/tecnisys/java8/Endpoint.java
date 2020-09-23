5bb20d00d10cddb2cd3404cdc17f7eb70cbff51d
Marcelo Nardelli <marcelo.nardelli@gmail.com>
2020-09-23 16:54:01 -0300

    retornando algo diferente

--- src/main/java/br/com/tecnisys/java8/Endpoint.java
+++ src/main/java/br/com/tecnisys/java8/Endpoint.java
@@ -8,6 +8,7 @@ public class Endpoint {
 
     @GetMapping
     public String versao(){
-        return System.getProperty("java.runtime.version");
+        //return System.getProperty("java.runtime.version");
+        return "Algo diferente";
     }
 }
